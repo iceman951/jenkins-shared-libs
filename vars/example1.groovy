ff1feb4215db47f086f0867193dd4d5119e8d494
Ice <47938031+iceman951@users.noreply.github.com>
2023-07-12 10:38:23 +0000

    test

--- vars/example1.groovy
+++ vars/example1.groovy
@@ -1,9 +1,8 @@
-// def call() {
-//     echo "example1 shared libs jenkins"
-// }
+def call() {
+    echo "example1 shared libs jenkins"
+    return this
+}
 
 def example2() {
     echo "echo example2 shared libs jenkins"
 }
-
-return this
