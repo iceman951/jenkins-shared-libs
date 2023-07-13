646b5b363d1cae8aa4a252e2d64a68863b25715b
Ice <47938031+iceman951@users.noreply.github.com>
2023-07-13 11:22:51 +0000

    naja

--- vars/example1.groovy
+++ vars/example1.groovy
@@ -6,3 +6,9 @@ def call() {
 def example2() {
     echo "echo example2 shared libs jenkins"
 }
+
+properties([
+    parameters([
+        [$class: 'DynamicReferenceParameter', name: 'Environment', choiceType: 'ET_FORMATTED_HTML', omitValueField: false, script: [$class: 'GroovyScript', script: [classpath: [], oldScript: '', sandbox: true, script: """ return "<h1> naja </h1>" """]]],
+    ])
+])
