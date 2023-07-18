2d3b89d409dfe5b11f9c68a9c79c63e6e911a943
Ice <47938031+iceman951@users.noreply.github.com>
2023-07-18 16:36:21 +0000

    params

--- vars/example1.groovy
+++ vars/example1.groovy
@@ -1,14 +1,16 @@
 def call() {
     echo "example1 shared libs jenkins"
-    properties([
-        parameters([
-            [$class: 'DynamicReferenceParameter', name: 'Environment', choiceType: 'ET_FORMATTED_HTML', omitValueField: false, script: [$class: 'GroovyScript', script: [classpath: [], oldScript: '', sandbox: true, script: """ return "<h1> naja </h1>" """]]],
-        ])
-    ])
-    parameters {
-        string(name: 'PARAM1', defaultValue: 'Default value 1', description: 'Parameter 1')
-        choice(name: 'PARAM2', choices: ['Option 1', 'Option 2', 'Option 3'], description: 'Parameter 2')
-    }
+    // properties([
+    //     parameters([
+    //         [$class: 'DynamicReferenceParameter', name: 'Environment', choiceType: 'ET_FORMATTED_HTML', omitValueField: false, script: [$class: 'GroovyScript', script: [classpath: [], oldScript: '', sandbox: true, script: """ return "<h1> naja </h1>" """]]],
+    //         [$class: 'DynamicReferenceParameter', name: 'GitURL', choiceType: 'ET_FORMATTED_HTML', omitValueField: false, script: [$class: 'GroovyScript', script: [classpath: [], oldScript: '', sandbox: true, script: """ return "${gitManifestRepoSsh}" """]]],
+
+    //     ])
+    // ])
+    // parameters {
+    //     string(name: 'PARAM1', defaultValue: 'Default value 1', description: 'Parameter 1')
+    //     choice(name: 'PARAM2', choices: ['Option 1', 'Option 2', 'Option 3'], description: 'Parameter 2')
+    // }
     booleanParam(name: 'RELOAD_CONFIG', defaultValue: false, description: 'if this option is selected deployment will be rolling update cause deployment to have a downtime')
     return this
 }
